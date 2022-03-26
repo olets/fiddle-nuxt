fd882731fa1f9bd703aa24a27cdcf9b814a08c2b
Henry Bley-Vroman <olets@olets.dev>
2022-03-26 09:28:30 -1000

    feat(tune): fetch and display tune data

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup>
+const id = Math.ceil(Math.random() * 99 + 1);
+const { data } = await useFetch(
+  `https://thesession.org/tunes/${id}?format=json`
+);
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Player
+    :name="data.name"
+    :setting="data.settings[0]"
+    :id="data.id"
+  ></Player>
 </template>
---
+++ components/Player.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>
+    <a :href="setting.url">{{ name }} ({{ id }})</a>
+  </h1>
+  
+  <textarea readonly>{{ tune }}</textarea>
+</template>
+
+<script>
+const tuneFromSetting = (setting, name) => {
+  const tune = `
+X:${setting.id}
+T:${name}
+K:${setting.key}
+${setting.abc.replace(/!/g, "\n")}`;
+
+  return tune;
+};
+
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    setting: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    tune() {
+      return tuneFromSetting(this.setting, this.name);
+    },
+  },
+};
+</script>
