a8c06f3be310eaf62fac50e3bb26b3704af3ac83
Henry Bley-Vroman <olets@olets.dev>
2022-03-26 11:09:31 -1000

    fix(fetch): useAsyncData instead of useFetch to resolve error...
    TheSession's API will sometimes return null.
    
    useFetch does not come with a way of handling the error,
    and the unhandled error was breaking event listeners.

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <script setup>
 const id = Math.ceil(Math.random() * 99 + 1);
-const { data } = await useFetch(
-  `https://thesession.org/tunes/${id}?format=json`
+const { data: tune } = await useAsyncData("myRandomKey", () =>
+  $fetch(`https://thesession.org/tunes/${id}?format=json`).catch((e) => {
+    console.log(e);
+  })
 );
 </script>
 
