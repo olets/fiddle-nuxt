b1dc4505544e13915c4c1f888dcf9e17cfb7c2e2
Henry Bley-Vroman <olets@olets.dev>
2022-03-26 13:05:00 -1000

    feat(tailwind): install and configure...
    This does not follow the current Tailwind CSS documentation, which is for Nuxt 2 only
    
    >>> pnpm install  -D tailwindcss
    >>> pnpm install --save-peer autoprefixer@^10.0.2
    >>> pnpm install --save-peer postcss@^8.1.0

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "app.vue",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
